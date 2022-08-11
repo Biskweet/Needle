2d58ca5c5235e4a24e79acef2c722dc0aeae03dd
gasparddm <gaspard.dama@gmail.com>
2022-08-11 15:23:58 +0200

    Made the victim generate as many values as needed in order no narrow the research more easily

--- victim.c
+++ victim.c
@@ -1,15 +1,30 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 
 int main() {
+    srand(time(NULL));
+
     char* mess = malloc(sizeof(char) * 20);
     sprintf(mess, "This is a test!");
-	printf("PID: %d, Message: \"%s\", Addresse: 0x%p\n", GetCurrentProcessId(), mess, mess);
 
-    int _;
-    printf("Now waiting..."); scanf("%d", &_);
+    int mystery;
+    char running = 1;
+
+    while (running) {
+        mystery = rand();
+    	  printf("PID: %lu, Message: \"%s\", Addresse: 0x%p\n", GetCurrentProcessId(), mess, mess);
+        printf("Mystery number: %d (size=%u bytes, address=0x%p).\n", mystery, sizeof(mystery), &mystery);
+
+        printf("Now waiting...\n");
+
+        char buf[2];
+        fgets(buf, 2, stdin);
+        if (buf[0] == 'x') running = 0;
+    }
 
     return 0;
 }
