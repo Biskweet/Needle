b66b52067050a3d194eb4c9a07bd5f931ab26612
gasparddm <gaspard.dama@gmail.com>
2022-08-25 18:08:10 +0200

    Improved the victim process

--- victim.c
+++ victim.c
@@ -16,14 +16,26 @@ int main() {
 
     while (running) {
         mystery = rand();
-    	  printf("PID: %lu, Message: \"%s\", Address: 0x%p\n", GetCurrentProcessId(), mess, mess);
+    	printf("PID: %lu, Message: \"%s\", Address: 0x%p\n", GetCurrentProcessId(), mess, mess);
         printf("Mystery number: %d (size=%llu bytes, address=0x%p).\n", mystery, sizeof(mystery), &mystery);
 
         printf("Now waiting...\n");
 
         char buf[2];
         fgets(buf, 2, stdin);
+
         if (buf[0] == 'x') running = 0;
+
+        // Re print the value to check if it's still the same
+        else while (buf[0] == 'r') {
+            printf("PID: %lu, Message: \"%s\", Address: 0x%p\n", GetCurrentProcessId(), mess, mess);
+            printf("Mystery number: %d (size=%llu bytes, address=0x%p).\n", mystery, sizeof(mystery), &mystery);
+
+            printf("Now waiting...\n");
+
+            char buf[2];
+            fgets(buf, 2, stdin);
+        }
     }
 
     return 0;
